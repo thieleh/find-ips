37dff34250c26e01c1e668e875b1fa137eac2a2a
thieleh <thiele_hemann@hotmail.com>
2023-08-08 19:25:34 -0300

    created method to find servers

--- app/app.go
+++ app/app.go
@@ -8,24 +8,32 @@ import (
 	"github.com/urfave/cli"
 )
 
-// Generate will return aplciation ready to be executed
+// Generate will return application ready to be executed
 func Generate() *cli.App {
 
 	app := cli.NewApp()
 	app.Name = "Command Line Application"
 	app.Usage = "Search IPs and Server names"
 
+	flags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "neopets.com",
+		},
+	}
+
 	app.Commands = []cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Search Ips",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "github.com/thieleh",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Search Ips",
+			Flags:  flags,
 			Action: findIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Find the name of the servers",
+			Flags:  flags,
+			Action: findServers,
 		},
 	}
 
@@ -44,3 +52,25 @@ func findIps(c *cli.Context) {
 		fmt.Println(ip)
 	}
 }
+
+func findServers(c *cli.Context) {
+	host := c.String("host")
+
+	servers, error := net.LookupNS(host)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host)
+	}
+}
+
+/*
+read-me to be created...
+to execute the application and find the ip:
+go run main.go ip --host [flag-host]
+
+to execute the application and find the server:
+go run main.go servers --host [flag-host]
+*/
